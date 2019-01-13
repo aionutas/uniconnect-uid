a286e6411bb0dd18263956351c9713606d1bac92
Budzugann <42567335+Budzugann@users.noreply.github.com>
2019-01-13 18:06:51 +0200

    done

--- uniconnect/src/app/app.module.ts
+++ uniconnect/src/app/app.module.ts
@@ -1,35 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import {CommonModule} from "@angular/common";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
-  MatButtonModule, MatCardModule, MatExpansionModule,
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
   MatFormFieldModule,
   MatInputModule,
-  MatListModule, MatOptionModule, MatSelectModule,
-  MatSnackBarModule
+  MatListModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatCheckboxModule,
+  MatRadioModule
 } from "@angular/material";
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatTreeModule} from '@angular/material/tree';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
-import { SurveyComponent } from './survey/survey.component';
-import { DriveComponent } from './drive/drive.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { ResourcesComponent } from './resources/resources.component';
-import {EventsComponent} from "./events/events.component";
-import {ACourseComponent} from "./aCourse/acourse.component";
-import {AskQuestionComponent} from "./askQuestion/askquestion.component";
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
+import { SurveyComponent } from "./survey/survey.component";
+import { DriveComponent } from "./drive/drive.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { ResourcesComponent } from "./resources/resources.component";
+import { EventsComponent } from "./events/events.component";
+import { ACourseComponent } from "./aCourse/acourse.component";
+import { AskQuestionComponent } from "./askQuestion/askquestion.component";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -44,7 +52,6 @@ import {AskQuestionComponent} from "./askQuestion/askquestion.component";
     EventsComponent,
     ACourseComponent,
     AskQuestionComponent
-
   ],
   imports: [
     BrowserModule,
@@ -70,9 +77,13 @@ import {AskQuestionComponent} from "./askQuestion/askquestion.component";
     MatFormFieldModule,
     MatOptionModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- uniconnect/src/app/survey/survey.component.ts
+++ uniconnect/src/app/survey/survey.component.ts
@@ -1,22 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-survey',
-  templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.css']
+  selector: "app-survey",
+  templateUrl: "./survey.component.html",
+  styleUrls: ["./survey.component.css"]
 })
 export class SurveyComponent implements OnInit {
-
-  display: boolean = false;
-
-  constructor() { }
-
+  showNumberOfAnswers = false;
+  showTypeOfAnswer = false;
+  generateAnswers: string[];
+  answerList;
+  form: FormGroup;
+  questionn: string;
+  savedQuestions;
+  constructor(private formBuilder: FormBuilder, public snackBar: MatSnackBar) {
+    this.form = this.formBuilder.group({
+      question: ["", ""],
+      numberOfAnswers: ["", ""],
+      typeOfAnswer: ["", ""]
+    });
+  }
   ngOnInit() {
+    this.savedQuestions = [];
+    this.showNumberOfAnswers = false;
+    this.showTypeOfAnswer = false;
+    this.answerList = [];
+  }
+  get question() {
+    return this.form.get("question").value;
   }
 
-  onConfirm() {
+  get numberOfAnswers() {
+    return this.form.get("numberOfAnswers").value;
+  }
 
+  get typeOfAnswer() {
+    return this.form.get("typeOfAnswer").value;
+  }
+  onQuestion() {
+    this.showNumberOfAnswers = true;
+    this.questionn = this.question;
+  }
+  onNumberOfAnswers() {
+    this.showTypeOfAnswer = true;
+  }
+  onTypeOfAnswer() {
+    this.generateAnswers = [];
+    let i;
+    for (i = 0; i < this.numberOfAnswers; i++) {
+      this.generateAnswers.push(this.typeOfAnswer);
+    }
+  }
+  onConfirm(type, input, index) {
+    const answer = {
+      type: type,
+      answer: input.value
+    };
+    this.generateAnswers.splice(index, 1);
 
-    this.display = true;
+    this.answerList.push(answer);
+  }
+  onShowAnswers() {
+    this.generateAnswers = undefined;
+  }
+  onSaveQuestion() {
+    const savedQuestions = {
+      question: this.question,
+      answerList: this.answerList
+    };
+    this.savedQuestions.push(savedQuestions);
+    this.generateAnswers = undefined;
+    this.answerList = [];
+  }
+  onSurveySubmit() {
+    this.snackBar.open("Survey Submitted !", "", {
+      duration: 2000
+    });
   }
 }
